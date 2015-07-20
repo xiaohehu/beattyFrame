e430ad7817e62a5e8da9f9802293d3c9ecb68abc
xiaohehu <xiaohe.hu@bos-mini-3.local>
2015-07-20 15:07:21 -0400

    Initial Commit

---
+++ beattyFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  beattyFrame
+//
+//  Created by Xiaohe Hu on 7/20/15.
+//  Copyright (c) 2015 Xiaohe Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ beattyFrame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  beattyFrame
+//
+//  Created by Xiaohe Hu on 7/20/15.
+//  Copyright (c) 2015 Xiaohe Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
