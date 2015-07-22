0256adc0b88d3ecab83bed7ecb4d829aeef5609f
xiaohehu <xiaohe.hu@bos-mini-3.local>
2015-07-22 17:01:14 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#This is basic framework of Beatty Harbor Point
---
+++ beattyFrame/SummaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  SummaryViewController.h
+//  beattyFrame
+//
+//  Created by Xiaohe Hu on 7/21/15.
+//  Copyright (c) 2015 Xiaohe Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SummaryViewController : UIViewController
+
+@end
