5856307ce8792989f7501bb90336621af6ef04c5
xiaohehu <xiaohe.hu@bos-mini-3.local>
2015-07-23 16:55:02 -0400

    Added Gallery Framework to load gallery

--- beattyFrame/GalleryViewController.m
+++ beattyFrame/GalleryViewController.m
@@ -8,10 +8,14 @@
 
 #import "GalleryViewController.h"
 #import "GalleryCollectionViewCell.h"
+#import <XHGalleryUIControls/XHGalleryUIControls.h>
 
-@interface GalleryViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
-
+@interface GalleryViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, XHGalleryDelegate>
+{
+    NSArray                     *arr_rawData;
+}
 @property (weak, nonatomic) IBOutlet UICollectionView *uic_collectionView;
+@property (strong, nonatomic)   XHGalleryViewController *gallery;
 
 @end
 
@@ -23,11 +27,19 @@
     
     _uic_collectionView.delegate = self;
     _uic_collectionView.dataSource = self;
+    [self prepareGalleryData];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)prepareGalleryData
+{
+    NSString *url = [[NSBundle mainBundle] pathForResource:@"photoData" ofType:@"plist"];
+    arr_rawData = [[NSArray alloc] initWithContentsOfFile:url];
+    NSLog(@"%@", arr_rawData);
 }
 
 #pragma mark - Gallery UICollection View
@@ -49,10 +61,23 @@
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-
+    
+    _gallery = [[XHGalleryViewController alloc] init];
+    _gallery.delegate = self;
+    _gallery.startIndex = 0; 		// Change this value to start with different page
+    _gallery.view.frame = self.view.bounds; 	// Change to load different frame
+    _gallery.arr_rawData = [arr_rawData objectAtIndex:0];
+    [self addChildViewController: _gallery];
+    [self.view addSubview: _gallery.view];
 }
 
-
+- (void)didRemoveFromSuperView
+{
+    [_gallery.view removeFromSuperview];
+    _gallery.view = nil;
+    [_gallery removeFromParentViewController];
+    _gallery = nil;
+}
 /*
 #pragma mark - Navigation
 
---
+++ beattyFrame/XHGalleryUIControls.framework/Versions/A/Headers/XHGalleryUIControls.h
@@ -0,0 +1,11 @@
+//
+//  XHGalleryUIControls.h
+//  XHGalleryUIControls
+//
+//  Created by Xiaohe Hu on 4/28/15.
+//  Copyright (c) 2015 Neoscape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XHGalleryUIControls/XHGalleryUIControls.h>
+#import <XHGalleryUIControls/XHGalleryViewController.h>
